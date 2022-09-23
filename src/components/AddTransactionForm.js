b08969463ebe8a104b12e14dd646b52fa1f6f29a
nelsonmutindamwanzia <nmutinda02@gmail.com>
2022-09-23 15:52:48 +0300

    add handleSubmit and handleChange functions to AddTransactionForm.js

--- src/components/AddTransactionForm.js
+++ src/components/AddTransactionForm.js
@@ -1,14 +1,22 @@
-import React from "react";
+import React, {useState} from "react";
 
 function AddTransactionForm() {
+  function handleChange(){
+
+  }
+
+  function handleSubmit(){
+
+  }
+
   return (
     <div className="ui segment">
       <form className="ui form">
         <div className="inline fields">
-          <input type="date" name="date" />
-          <input type="text" name="description" placeholder="Description" />
-          <input type="text" name="category" placeholder="Category" />
-          <input type="number" name="amount" placeholder="Amount" step="0.01" />
+          <input onChange = {handleChange} type="date" name="date" />
+          <input onChange = {handleChange} type="text" name="description" placeholder="Description" />
+          <input onChange = {handleChange} type="text" name="category" placeholder="Category" />
+          <input onChange = {handleChange} type="number" name="amount" placeholder="Amount" step="0.01" />
         </div>
         <button className="ui button" type="submit">
           Add Transaction
